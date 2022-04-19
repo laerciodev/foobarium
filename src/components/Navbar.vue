384414b0e00b6e59f5f34fcd1e32f95cf4b574b2
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-19 16:43:28 -0300

    chore: migrate to vite

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>Navbar</div>
+  <div>
+    <LogoSVG />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import LogoSVG from '@/assets/logo.svg';
+
+export default defineComponent({
+  components: {
+    LogoSVG,
+  },
+});
+</script>
