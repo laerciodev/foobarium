a29dd337737a88a4d06ae59e0228db51aa2d016e
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-21 17:41:35 -0300

    feat: add router-view in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
-  <Sidebar />
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Sidebar from '@/components/Sidebar.vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  components: {
-    Sidebar,
+  setup() {
+
   },
-});
+})
 </script>
