3c8c8cce4c63e23e0b2af2597dcb05d525ad31ea
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-21 18:42:25 -0300

    feat: create Posts view

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-home">
+    <Sidebar />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Sidebar from '@/components/Sidebar'
+
+export default defineComponent({
+  components: {
+    Sidebar
+  },
+  setup() {
+
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.container-home {
+  display: flex;
+  height: 100%;
+}
+</style>
