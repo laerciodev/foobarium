c8a89a6cbc1e72367841d832a425ad95985e84f2
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-22 06:38:34 -0300

    feat: create TitlePost component

---
+++ src/components/TitlePost.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 class="title">
+    Cervus sequi vergo advoco textor demum capitulus conqueror
+    quam voluptas crudelis.
+  </h2>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  margin: 30px 0;
+  color: #fff;
+  font-size: 24px;
+  line-height: 29px;
+  text-transform: uppercase;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 700;
+  letter-spacing: -0.1em;
+  font-style: bold;
+}
+</style>
