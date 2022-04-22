4d36fb3327a5038338f0a8f1f6063f16e710e026
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-22 06:37:42 -0300

    feat: create ArticlePost component

---
+++ src/components/ArticlePost.vue
@@ -0,0 +1,19 @@
+<template>
+  <article class="article">
+    Exercitationem thymbra angelus.
+    Deripio comminor vel.
+    Urbanus incidunt carbo.
+    Creator admoveo pectus.
+    Centum cultellus turpe. Suggero ...
+  </article>
+</template>
+
+<style lang="scss" scoped>
+.article {
+  color: #EEE;
+  font-family: 'Oxygen', sans-serif;
+  font-size: 16px;
+  line-height: 20px;
+  margin-bottom: 30px;
+}
+</style>
