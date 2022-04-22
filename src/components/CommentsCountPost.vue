d39a19404fbb67fdfdd40463c5e8d69fe52e011b
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-22 06:38:09 -0300

    feat: create CommentsCountPost component

---
+++ src/components/CommentsCountPost.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-comments-count">
+    <div class="comments-count-badge">12</div>
+    <span class="comments-text">comments</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container-comments-count {
+  display: flex;
+  align-items: center;
+  gap: 2px;
+  cursor: pointer;
+}
+
+.comments-count-badge {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 26px;
+  height: 26px;
+  border-radius: 50%;
+  background: #3B3D43;
+  color: #FFF;
+  font-family: 'Oxygen', sans-serif;
+  font-weight: 700;
+}
+
+.comments-text {
+  color: #FFF;
+  font-size: 16px;
+  line-height: 20px;
+  font-family: 'Oxygen', sans-serif;
+  font-weight: 400;
+}
+</style>
