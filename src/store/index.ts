60a747c7442c4b8e2dced90919a94e8a8151cded
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-23 08:39:28 -0300

    feat: create getPosts action

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
@@ -6,6 +7,10 @@ export default createStore({
   mutations: {
   },
   actions: {
+    async getPosts() {
+      const { data: posts } = await axios.get('https://gorest.co.in/public/v2/posts');
+      return posts;
+    }
   },
   modules: {
   },
