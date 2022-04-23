ba602d1b45c83d90e9e9ba6e61f4bb9e406024a2
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-23 08:37:11 -0300

    feat: create PostItem component

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,10 +1,10 @@
 <template>
-  <li class="container-post">
+  <div class="post-item-container">
     <UsernamePost />
-    <TitlePost />
+    <TitlePost :title="post.title" />
     <ArticlePost />
     <CommentsCountPost />
-  </li>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -12,18 +12,23 @@ import UsernamePost from '@/components/UsernamePost.vue';
 import TitlePost from '@/components/TitlePost.vue';
 import ArticlePost from '@/components/ArticlePost.vue';
 import CommentsCountPost from '@/components/CommentsCountPost.vue';
+import { Post } from '@/types';
+
+const props = defineProps<{
+  post: Post,
+  index?: number,
+}>();
 
 </script>
 
-
 <style lang="scss" scoped>
-.container-post {
-  width: 100%;
-  max-width: 413px;
+.post-item-container {
+  width: 240px;
+  flex-grow: 1;
+  min-width: 25%;
   background: #61646D;
-  border-radius: 15px;
-  padding: 24px 30px 54px 30px;
   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.25);
-  cursor: pointer;
+  border-radius: 15px;
+  padding: 23px 30px 24px 30px;
 }
 </style>
