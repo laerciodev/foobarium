37804553b5827cadda970313e508cf17b6f0b473
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-24 18:43:31 -0300

    feat: create Comment Interface

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,23 +8,36 @@ const TOKEN = '0b8d4c47d78dbdcfcc1b84d457da0bc61e958e39dac4a182e5c230a57f9ce227'
 export default createStore({
   state: {
     posts: [],
+    comments: []
   },
   mutations: {
-    SET_POSTS: (state, payload) => state.posts = payload
+    SET_POSTS: (state, payload) => state.posts = payload,
+    SET_COMMENTS: (state, payload) => state.comments = payload,
   },
   getters: {
     getPosts: (state) => chunkArray(state.posts, 3),
     getPostById: (state) => (id) => state.posts.find(
       post => post.id === id
-    )
+    ),
+    getComments: (state) => state.comments,
   },
   actions: {
-    async fetchPosts({ commit, dispatch }) {
+    async fetchPosts({ commit }) {
       const url = `${BASE_URL}/posts`;
       const { data: posts } = await axios.get(url);
       commit('SET_POSTS', posts);
     },
-    async addComment({ dispatch }, payload) {
+    async fetchCommentsByPost({ commit }, id) {
+      const url = `${BASE_URL}/posts/${id}/comments`;
+      const headers = {
+        Authorization: `Bearer ${TOKEN}`,
+        Accept: 'application/json',
+        'Content-type': 'application/json'
+      };
+      const { data: comments } = await axios.get(url, { headers });
+      commit('SET_COMMENTS', comments);
+    },
+    async addComment(payload) {
       const url = `${BASE_URL}/posts/${payload.post_id}/comments`;
       const headers = {
         Authorization: `Bearer ${TOKEN}`,
--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,11 @@ export interface Post {
   title: string,
   user_id: number,
 }
+
+export interface Comment {
+  id: number,
+  post_id: number,
+  name: string,
+  email: string,
+  body: string,
+}
