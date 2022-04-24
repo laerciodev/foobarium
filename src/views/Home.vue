107717f090aac4a878e6132750156ecb5b56b87d
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-24 08:38:38 -0300

    feat: refactor Home views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="container-home">
     <Sidebar />
-    <Suspense>
+    <main class="main">
       <router-view></router-view>
-    </Suspense>
+    </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed, onMounted } from 'vue';
+import { useStore } from 'vuex';
 import Sidebar from '@/components/Sidebar.vue';
 import PostList from '@/views/PostList.vue';
+
+const store = useStore();
+const posts = computed(() => store.getters.getPosts);
+
+onMounted(async () => {
+  await store.dispatch('fetchPosts');
+});
 
 </script>
 
@@ -18,4 +27,13 @@ import PostList from '@/views/PostList.vue';
   display: flex;
   height: 100%;
 }
+
+.main {
+  width: calc(100% - 433px);
+  height: 100%;
+  background: #41434B;
+  padding: 50px 60px 0 60px;
+  overflow-y: auto;
+}
+
 </style>
