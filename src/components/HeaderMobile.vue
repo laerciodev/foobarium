1f4895b5472b6da8462841ea826e53f6e8bfe784
Laércio <jose.laercio.bezerra@gmail.com>
2022-04-25 07:01:18 -0300

    feat: hide back button at HeaderMobile component

--- src/components/HeaderMobile.vue
+++ src/components/HeaderMobile.vue
@@ -1,15 +1,28 @@
 <template>
   <header class="header-mobile-container">
-    <BackButtonIcon class="back-button-icon" />
+    <BackButtonIcon
+      :class="['back-button-icon', isHome && 'hide-back-button']"
+    />
     <Logo width="235px" height="30px" />
     <MenuIcon class="menu-icon" />
   </header>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
 import BackButtonIcon from '@/components/BackButtonIcon.vue';
 import Logo from '@/components/Logo.vue';
 import MenuIcon from '@/components/MenuIcon.vue';
+
+const route = useRoute();
+const isHome = ref(false);
+
+watch(
+  () => route.path,
+  () => isHome.value = route.path === '/',
+  { immediate: true }
+)
 
 </script>
 
@@ -28,6 +41,10 @@ import MenuIcon from '@/components/MenuIcon.vue';
 
   .back-button-icon {
     margin-left: 22px;
+  }
+
+  .hide-back-button {
+    visibility: hidden;
   }
 
   .menu-icon {
